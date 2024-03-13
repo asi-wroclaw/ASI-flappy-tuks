44d00672430d35850a465fb82611436f301c70a7
Das15 <62486730+Das15@users.noreply.github.com>
2024-03-13 16:31:43 +0100

    Added basic object number scaling.

--- FlappyTux/Main.gd
+++ FlappyTux/Main.gd
@@ -5,21 +5,38 @@ export (Array, PackedScene) var bergTemplates;
 export (float) var start_speed=150;
 export (float) var speed_increase=10;
 export (float) var speed_randomization=100;
+export (float) var spawn_delay=3;
+export (float) var min_spawn_delay=0.4;
+export (float) var spawn_delay_change=8;
 
 var speed=0;
+var next_spawn_time=0;
+var last_spawn_time_change;
 
 func _init():
 	speed=start_speed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	last_spawn_time_change = Time.get_unix_time_from_system()
+
+# 
+func get_spawning_cooldown():
+	return 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	speed+=speed_increase*delta
-
-func _on_Obstangle_Timer_timeout():
+	var curr_time = Time.get_unix_time_from_system()
+	if next_spawn_time < curr_time:
+		spawn_iceberg()
+		next_spawn_time = curr_time + spawn_delay
+		if last_spawn_time_change + spawn_delay_change < curr_time:
+			spawn_delay -= 0.1
+		if spawn_delay < min_spawn_delay:
+			spawn_delay = min_spawn_delay
+		 
+func spawn_iceberg():
 	var berg=bergTemplates[randi()%bergTemplates.size()].instance()
 	add_child(berg)
 	
@@ -34,3 +51,6 @@ func _on_Obstangle_Timer_timeout():
 	berg.linear_velocity = velocity.rotated(direction)
 	
 	berg.rotation = rand_range(0, 2*PI);
+
+func _on_Obstangle_Timer_timeout():
+	pass
