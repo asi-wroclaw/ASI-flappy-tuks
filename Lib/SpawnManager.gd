d554bae82da73446d1db5610355b043f3847e226
BigflipBOT <filip.pyra@gmail.com>
2024-03-22 16:46:42 +0100

    skrypt do piotrowawrzyniakowania obiektów kier lodowych robienia ładnie 1st commit

---
+++ Lib/SpawnManager.gd
@@ -0,0 +1,20 @@
+extends Node
+var object_treshold = 30; #TODO tweak this value
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var object_pointer_dict = {"key": "classname"}
+	pass # Replace with function body.
+
+public func obj_add(): # add object to dictionary ???
+	pass
+
+func delete_object():
+	pass
+	
+func create_object():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
