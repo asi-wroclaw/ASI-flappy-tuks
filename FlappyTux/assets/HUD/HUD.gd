cfadc0e2b170751cbab9efea63391d6064441517
Das15 <62486730+Das15@users.noreply.github.com>
2024-03-22 16:36:52 +0100

    Added a few QOL features to score display.

--- FlappyTux/assets/HUD/HUD.gd
+++ FlappyTux/assets/HUD/HUD.gd
@@ -2,6 +2,8 @@ extends CanvasLayer
 
 var you_died=false
 var score=0
+var delta_timer = 0
+export (float) var score_update_interval
 
 func _ready():
 	$DiedText.hide()
@@ -12,6 +14,8 @@ func died():
 
 func _process(delta):
 	if not you_died:
-		score+=delta
-	
-	$Scores.text="Score: "+str(int(score))
+		score+=delta*100
+	delta_timer += delta
+	if (delta_timer > score_update_interval):
+		delta_timer = 0
+		$Scores.text="Score: "+str(int(score))
